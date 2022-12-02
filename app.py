9faa3fbd83252cc032c76c5cf73dfaa7a2a52f30
Nitish Chaturvedi <85406812+waterupto@users.noreply.github.com>
2022-12-03 04:27:11 +0530

    init: initiate FastAPI

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import Flask, redirect, render_template, request, url_for
 import time 
 from urlgen import make_api_url
 from functions import execute_query, get_query_status, get_query_results, cancel_query_execution
+from fastapi import FastAPI
 
 
 API_key = keys()
@@ -16,7 +17,7 @@ BASE_URL = "https://api.dune.com/api/v1/"
 
 app = Flask(__name__)
 
-
+app = FastAPI()
 
 
 
