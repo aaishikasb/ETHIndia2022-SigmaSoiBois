576bd11d5060d418a83f7e050a61849c5f63c7e3
Nitish Chaturvedi <85406812+waterupto@users.noreply.github.com>
2022-12-03 00:41:17 +0530

    chore: header func

--- credentials.py
+++ credentials.py
@@ -2,5 +2,9 @@
 def keys():
     API_KEY = "" 
     HEADER= {"Dune key Header" : API_KEY}
-    return API_KEY 
+    return API_KEY
 
+def header():
+    API_KEY = "" 
+    HEADER= {"Dune key Header" : API_KEY}
+    return HEADER
