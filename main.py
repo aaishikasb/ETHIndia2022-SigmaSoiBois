298926a6e24c1c28d9a6edae833802ccb70728d3
Nitish Chaturvedi <85406812+waterupto@users.noreply.github.com>
2022-12-03 06:23:40 +0530

    wip: python api

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/functions.py/{query_id}")
+@app.get("analytics/{query_id}")
 def execute_query(query_id: int):
     return {"execution_id": execution_id}
 
-@app.get("/functions.py/{execution_id}")
-def get_query_status(execution_id: int):
+@app.get("analytics/{response}")
+def get_query_status(execution_id: any):
     return {"response": execution_id}
 
-@app.get("/functions.py/{execution_id}")
-def get_query_results(execution_id: int):
+@app.get("analytics/{response}")
+def get_query_results(execution_id: any):
     return {"response": item_id}
 
-@app.get("/functions.py/{execution_id}")
-def cancel_query_execution(execution_id: int):
+@app.get("/{response}")
+def cancel_query_execution(execution_id: any):
     return {"response": execution_id}
