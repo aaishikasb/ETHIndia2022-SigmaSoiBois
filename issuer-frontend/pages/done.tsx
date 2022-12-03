d5d4a82468cb3aa6bf4f79199ab15694e122f7a6
aaishikasb <mail.aaishika@gmail.com>
2022-12-03 21:34:43 +0530

    Add States

---
+++ issuer-frontend/pages/done.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>SigmaZK</title>
+        <meta name="description" content="Polygon ID integration with Collab.Land" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <span className={styles.polyGrad}>SigmaZK</span>
+        </h1>
+
+        <h1 className={styles.emoji}>✅</h1>
+
+        <h1 className={styles.subtitle}>Successfully created offer claim!</h1>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://collab.land"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
