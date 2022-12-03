ec34e5ce1af5f092180f07f3a42739aadc1b39cd
Gyanesh Samanta <mail.gyaneshsamanta@gmail.com>
2022-12-03 08:39:46 +0530

    feat: Dune API Complete

--- python-api/main.py
+++ python-api/main.py
@@ -1,22 +1,76 @@
-from typing import Union
+import os
 
-from fastapi import FastAPI
+from flask import Flask, redirect, render_template, request, url_for
 
-app = FastAPI()
+from requests import get, post
+import pandas as pd
+import time
+
+API_KEY = os.getenv("API_KEY")
+HEADER = {"x-dune-api-key" : API_KEY}
+
+# Example Query ID - 638435
+QUERY_ID = "your_query_ID"
+
+BASE_URL = "https://api.dune.com/api/v1/"
 
 
-@app.get("analytics/{query_id}")
-def execute_query(query_id: int):
-    return {"execution_id": execution_id}
+app = Flask(__name__)
 
-@app.get("analytics/{response}")
-def get_query_status(execution_id: any):
-    return {"response": execution_id}
 
-@app.get("analytics/{response}")
-def get_query_results(execution_id: any):
-    return {"response": item_id}
+def make_api_url(module, action, ID):
+    """
+    We shall use this function to generate a URL to call the API.
+    """
 
-@app.get("/{response}")
-def cancel_query_execution(execution_id: any):
-    return {"response": execution_id}
+    url = BASE_URL + module + "/" + ID + "/" + action
+
+    return url
+
+
+def execute_query_with_params(query_id, param_dict):
+    """
+    Takes in the query ID. And a dictionary containing parameter values.
+    Calls the API to execute the query.
+    Returns the execution ID of the instance which is executing the query.
+    """
+
+    url = make_api_url("query", "execute", query_id)
+    response = post(url, headers=HEADER, json={"query_parameters" : param_dict})
+    execution_id = response.json()['execution_id']
+
+    return execution_id
+
+
+def get_query_results(execution_id):
+    """
+    Takes in an execution ID.
+    Fetches the results returned from the query using the API
+    Returns the results response object
+    """
+
+    url = make_api_url("execution", "results", execution_id)
+    response = get(url, headers=HEADER)
+
+    return response
+
+
+def fetch_data_from_chains(address):
+
+    parameters = {"wallet_address" : address}
+    execution_id = execute_query_with_params(QUERY_ID, parameters)
+    time.sleep(10)
+    response = get_query_results(execution_id)
+
+    return response.json()['result']['rows']['sum']
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        address = request.form["address"]
+        response = fetch_data_from_chains(address)
+        return redirect(url_for("index", result=response))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
