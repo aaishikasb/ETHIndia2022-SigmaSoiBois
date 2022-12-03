08d55af5c49b79ce0eae5902c5ce5b16850a4a66
aaishikasb <mail.aaishika@gmail.com>
2022-12-03 22:04:59 +0530

    Fix Index

--- issuer-frontend/pages/index.tsx
+++ issuer-frontend/pages/index.tsx
@@ -6,22 +6,26 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Polygon ID - Collab.Land</title>
+        <title>SigmaZK</title>
         <meta name="description" content="Polygon ID integration with Collab.Land" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <span className={styles.polyGrad}>Polygon ID</span> integration with <span className={styles.collabYellow}>Collab.</span><span className={styles.collabGrey}>Land</span> Demo!
+          Welcome to <span className={styles.polyGrad}>SigmaZK</span>!
         </h1>
 
+        <h1 className={styles.subtitle}>Proof-of-Work in a zk-way.</h1>
+
+        <p className={styles.description}>Curate Claims or Verifiable Credentials effortlessly for contributors in your community and give them the power of self sovereign identify (SSI). They can use these claims in the communities as Zero Knowledge way of verifying their work. </p>
         <p className={styles.description}>
           1. Get started by creating an organiztion.<br />
           2. Defining a Schema for your Verifiable Credential.<br />
           3. Issuing a Claim for a Verifiable Credential to a user.<br />
           4. User Claims the verifiable credential!<br />
         </p>
+        <button className={styles.submit2} type="submit">Checkout now! 👀</button>
       </main>
 
       <footer className={styles.footer}>
